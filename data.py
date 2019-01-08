044f2ea22262e1cb06eb9c58e2d6851fd1d39f45
Champ.art.website <32265715+champseo2017@users.noreply.github.com>
2019-01-09 04:09:33 +0700

    update

---
+++ data.py
@@ -0,0 +1,25 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title': 'Article One',
+            'body': 'The microframework Flask is based on the Pocoo projects Werkzeug and Jinja2. Werkzeug is a utility library for the Python programming language, in other words a toolkit for Web Server Gateway Interface (WSGI) applications, and is licensed under a BSD License.',
+            'author': 'Brad Traversy',
+            'create_date': '04-25-2017'
+        },
+        {
+            'id': 2,
+            'title': 'Article Two',
+            'body': 'The microframework Flask is based on the Pocoo projects Werkzeug and Jinja2. Werkzeug is a utility library for the Python programming language, in other words a toolkit for Web Server Gateway Interface (WSGI) applications, and is licensed under a BSD License.',
+            'author': 'Brad Traversy',
+            'create_date': '04-25-2017'
+        },
+         {
+            'id': 3,
+            'title': 'Article Tree',
+            'body': 'The microframework Flask is based on the Pocoo projects Werkzeug and Jinja2. Werkzeug is a utility library for the Python programming language, in other words a toolkit for Web Server Gateway Interface (WSGI) applications, and is licensed under a BSD License.',
+            'author': 'Brad Traversy',
+            'create_date': '04-25-2017'
+        }
+    ]
+    return articles
